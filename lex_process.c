87104f55d013ac0c2a63c67183d5c1b48de7b2d7
apenasgabi <gabriela.ap@sempreceub.com>
2025-03-26 21:25:20 -0300

    adicionar

--- lex_process.c
+++ lex_process.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 
 
-struct lex_process * lex_process_create(struct compile_process* compiler, struct lex_process_functions* functions, void *private){
-    struct lex_process* process = calloc(1,sizeof(struct token));
+struct lex_process* lex_process_create(struct compile_process* compiler, struct lex_process_functions* functions, void *private){
+    struct lex_process* process = calloc(1,sizeof(struct lex_process));
 
     process->function = functions;
     process->token_vec = vector_create(sizeof(struct token));
