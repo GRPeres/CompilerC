d803ce2922f847e9ada75227c252296e88f2c21b
GabrielMousinho <g.mousinho@sempreceub.com>
2025-04-09 20:13:49 -0300

    comentando

--- lexer.c
+++ lexer.c
@@ -184,6 +184,7 @@ struct token* read_next_token(){
         token = token_make_string();
         break;
 
+
     case '\n':
         token = token_create(&(struct token){.type=TOKEN_TYPE_NEWLINE});
         printf("Token: New Line \n");
